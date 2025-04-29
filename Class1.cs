f30b8457205f67adc94f3372b3e21da37a27044b
Taylo <Taylo@MARIEL>
2025-04-29 13:32:32 -0600

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_4._1._2
+{
+    internal class Calculator : ICalculate
+    {
+        public double Add(double x, double y)
+        {
+            return x + y;
+        }
+        public double Subtract(double x, double y)
+        {
+            return x - y;
+        }
+        public double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+        public double Divide(double x, double y)
+        {
+            return x / y;
+        }
+    }
+}
---
+++ ICalculate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_4._1._2
+{
+    internal interface ICalculate
+    {
+        double Add(double x, double y);
+        double Subtract(double x, double y);
+        double Multiply(double x, double y);
+        double Divide(double x, double y);
+
+    }
+}
